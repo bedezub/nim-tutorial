0ccac0ce11df9a91a8bf69c5df4a155502e33ed1
zubquzaini <zubliquzaini@gmail.com>
2021-03-01 21:54:38 +0800

    first commit

---
+++ greetings.nim
@@ -0,0 +1,9 @@
+# ! Thunder strike the pixel by BEDEZUB
+# ! Nim tutorial part 1:
+# ! The first program
+
+# This is a comment
+echo "What is your name? "
+var name: string = readLine(stdin)
+echo "Hi, ", name, "!"
+
